560694b0723ea4970131cdce582406056de10bb0
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-07-02 18:20:38 +0800

    added user schema

--- Backend/database/connect.js
+++ Backend/database/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 
 const connectDB = async (url) => {
-    // try {
-    //     await mongoose.connect(process.env.MONGO_URI)
-    //     console.log("Connected Succesfully");
-    // } catch (error) {
-    //     console.log("Connection Failed");
-    // }
-
-    return mongoose.connect(url)
+   try {
+     return mongoose.connect(url)
+   } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message)
+        process.exit(1)
+   }
 }
 
 module.exports = connectDB
