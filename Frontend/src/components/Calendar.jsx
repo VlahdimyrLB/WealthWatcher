51525b2943e545a31a3720baa9402a0d301efb81
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-07-21 00:22:48 +0800

    populated the calendar with transaction data

--- Frontend/src/components/Calendar.jsx
+++ Frontend/src/components/Calendar.jsx
@@ -5,21 +5,40 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import multiMonthPlugin from "@fullcalendar/multimonth";
 
-const Calendar = () => {
+const Calendar = ({ myEvents, onEventClick }) => {
+  const renderEventContent = (eventInfo) => {
+    const { category, amount } = eventInfo.event.extendedProps;
+    return (
+      <div className="fc-event-title">
+        {/* ${category} */}
+        <span className="text-sm">{`₱${parseFloat(amount).toLocaleString(
+          "en-US",
+          { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+        )}`}</span>
+      </div>
+    );
+  };
+
+  const eventClassNames = (eventInfo) => {
+    return eventInfo.event.extendedProps.type === "Income"
+      ? "text-blue-600"
+      : "text-red-600";
+  };
+
   return (
     <div className="mb-10">
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin]}
         initialView="dayGridMonth"
-        events={[
-          { title: "Event #1", start: "2024-07-19" },
-          { title: "Event #2", start: "2024-07-21" },
-        ]}
+        events={myEvents}
         headerToolbar={{
           left: "today prev,next",
           right: "dayGridMonth,timeGridWeek,listDay,multiMonthYear",
           center: "title",
         }}
+        eventContent={renderEventContent} // custom render function
+        eventClick={onEventClick} // handle event clicks
+        eventClassNames={eventClassNames} // custom class names based on type
       />
     </div>
   );
