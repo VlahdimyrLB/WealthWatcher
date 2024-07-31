6961031928c84a63dc9265104fa179d5fb986c86
VlahdimyrLB <bambalanvlahdimyr@gmail.com>
2024-07-31 20:25:06 +0800

    some ui changes

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { ModeToggle } from "@/components/theme/mode-toggle";
 
 const HomePage = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,20 +12,20 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-between space-y-6 mt-10">
+    <div className="flex flex-col items-center justify-between space-y-6 lg:mt-10">
       <p
-        className={`text-6xl font-bold text-center transition-opacity duration-500 ease-in-out ${
+        className={`lg:text-6xl text-2xl font-bold text-center transition-opacity duration-500 ease-in-out ${
           isVisible ? "opacity-100" : "opacity-0"
         }`}
       >
         A Comprehensive Finance Tracking Tool
       </p>
       <h4
-        className={`text-lg text-gray-300 transition-opacity duration-1000 ease-in-out ${
+        className={`lg:text-lg text-xs  font-semibold  dark:text-gray-300 transition-opacity duration-1000 ease-in-out ${
           isVisible ? "opacity-100" : "opacity-0"
         }`}
       >
-        Maglalagay tayo simple details dito about sa app
+        Simple app details and chuchenes here
       </h4>
     </div>
   );
